cfdc2280f5386a5e8e6e11688f01b594d72c3321
Ali <muhammadali90995@gmail.com>
2022-06-22 20:20:08 +0500

    8.20 pm

--- app/src/main/java/com/apps/timemanagmentapp/Activities/LoginActivity.java
+++ app/src/main/java/com/apps/timemanagmentapp/Activities/LoginActivity.java
@@ -1,19 +1,78 @@
 package com.apps.timemanagmentapp.Activities;
 
+import static com.apps.timemanagmentapp.Activities.RegisterActivity.mainurl;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.apps.timemanagmentapp.R;
+import com.apps.timemanagmentapp.databinding.ActivityLoginBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
+    ActivityLoginBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+
     }
+
+    public void addmeeting() {
+
+        if (binding.edLoginEmail.getText() == null ||
+                binding.edLoginPass.getText() == null) {
+            Toast.makeText(getApplicationContext(), "Please fill all fields", Toast.LENGTH_LONG).show();
+        } else {
+            String url = mainurl + "App/Verify?"+binding.edLoginEmail+"&pass="+binding.edLoginPass;
+            Log.d("urlCreate", url);
+
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            JsonObjectRequest request = new JsonObjectRequest(
+                    Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    SharedPreferences saveinfo=getSharedPreferences("savelogin",MODE_PRIVATE);
+                    SharedPreferences.Editor edit=saveinfo.edit();
+                    try {
+                        edit.putInt("id",response.getInt("id"));
+                        edit.putString("name",response.getString("name"));
+                        edit.apply();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+            );
+            requestQueue.add(request);
+        }
+    }
+
 }
--- app/src/main/java/com/apps/timemanagmentapp/Activities/RegisterActivity.java
+++ app/src/main/java/com/apps/timemanagmentapp/Activities/RegisterActivity.java
@@ -2,13 +2,27 @@ package com.apps.timemanagmentapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.apps.timemanagmentapp.R;
+import com.apps.timemanagmentapp.databinding.ActivityRegisterBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Objects;
 
@@ -16,11 +30,14 @@ public class RegisterActivity extends AppCompatActivity {
 
     StringBuilder builder = new StringBuilder();
     TextView textView;
+    ActivityRegisterBinding binding;
+     static String mainurl = "http://192.168.0.102/workScheduleApp/api/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         Objects.requireNonNull(getSupportActionBar()).hide();
         textView = findViewById(R.id.tv_login);
 
@@ -35,5 +52,48 @@ public class RegisterActivity extends AppCompatActivity {
         redSpannable.setSpan(new ForegroundColorSpan(Color.BLACK), 0, blue.length(), 0);
         builder.append(redSpannable);
         textView.setText(builder, TextView.BufferType.SPANNABLE);
+
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addmeeting();
+            }
+        });
+    }
+
+    public void addmeeting() {
+
+        if (binding.edName.getText() == null || binding.edRegisterEmail.getText() == null ||
+                binding.edRegisterPass.getText() == null) {
+            Toast.makeText(getApplicationContext(), "Please fill all fields", Toast.LENGTH_LONG).show();
+        } else {
+            String url = mainurl + "App/Save";
+            Log.d("urlCreate", url);
+            JSONObject Object = new JSONObject();
+            try {
+                Object.put("name", binding.edName.getText().toString());
+                Object.put("email", binding.edRegisterEmail.getText().toString());
+                Object.put("password", binding.edRegisterPass.getText().toString());
+
+            } catch (Exception ex) {
+                Log.d("error", ex.getMessage().toString());
+            }
+            Log.d("meetingObj", Object.toString());
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            JsonObjectRequest request = new JsonObjectRequest(
+                    Request.Method.POST, url, Object, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+            );
+            requestQueue.add(request);
+        }
     }
 }
